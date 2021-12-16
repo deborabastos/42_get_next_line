87a949d084030f6c720c69b5120c80a2375285d7
deborabastos <deborabastos14@gmail.com>
2021-12-16 16:09:39 -0300

    changed main

--- main.c
+++ main.c
@@ -6,12 +6,14 @@
 /*   By: dalves-p <dalves-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/13 20:54:29 by dalves-p          #+#    #+#             */
-/*   Updated: 2021/12/16 16:07:24 by dalves-p         ###   ########.fr       */
+/*   Updated: 2021/12/16 16:09:33 by dalves-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// Para compilar:
-// clang get_next_line_utils.c get_next_line.c main.c -D BUFFER_SIZE=50 && ./a.out
+/* 
+Para compilar:
+clang get_next_line_utils.c get_next_line.c main.c -D BUFFER_SIZE=50 && ./a.out
+*/
 
 #include <unistd.h>
 #include <stdlib.h>
@@ -25,14 +27,14 @@ int	main(void)
 	int		fd;
 	char	*line;
 
-	// fd = open("song.txt", O_RDONLY);
-	// get_next_line(fd, &line);
-	// printf("%s\n", line);
+	/* 
+	fd = open("song.txt", O_RDONLY);
+	get_next_line(fd, &line);
+	printf("%s\n", line);
 	
-
-	// get_next_line(fd, &line);
-	// printf("%s\n", line);
-
+	get_next_line(fd, &line);
+	printf("%s\n", line);
+	*/
 	fd = open("song.txt", O_RDONLY);
 	while (get_next_line(fd, &line) > 0)
 	{
